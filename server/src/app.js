0a1870d13a25cc0a0525aac1bead75dba1e6132b
Lincoln de Sousa <lincoln@comum.org>
2018-02-24 17:23:37 -0500

    server: Enable CORS for the website address

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import cors from 'cors';
 import graphqlHTTP from 'express-graphql';
 
 import * as authControllers from './controllers/auth';
-import { isEnv } from './config';
+import config, { isEnv } from './config';
 import { checkAuth } from './middleware/auth';
 import { Schema, Root } from './graphql';
 
@@ -14,6 +15,7 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(checkAuth({ except: ['/signin'] }));
+app.use(cors({ origin: config.site_url, optionsSuccessStatus: 200 }));
 app.post('/signin', authControllers.signin);
 app.get('/signin', authControllers.signin);
 app.use('/graphql', graphqlHTTP({
