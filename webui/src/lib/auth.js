ad57bee58b63ae1ac65d9321da8c0141f2769ddd
Lincoln de Sousa <lincoln@comum.org>
2018-02-24 18:12:33 -0500

    client: Error handling on sign in

--- webui/src/lib/auth.js
+++ webui/src/lib/auth.js
@@ -1,17 +1,21 @@
+const AUTH_TOKEN_KEY = 'authToken';
+
+async function handleResponse(response) {
+  if (response.status != 200) {
+    throw new Error(await response.text());
+  }
+}
+
 export async function signIn(email) {
   const fetchOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email }),
   };
-  try {
-    const response = await fetch('http://localhost:8001/signin', fetchOptions);
-    return response.json();
-  } catch (error) {
-    console.log(error);
-  }
+  return handleResponse(
+    await fetch('http://localhost:8001/signin', fetchOptions));
 }
 
 export function isLoggedIn() {
-  return !!window.localStorage.authToken;
+  return !!window.localStorage.getItem(AUTH_TOKEN_KEY);
 }
--- webui/src/screens/SignIn.js
+++ webui/src/screens/SignIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Alert,
   Grid,
   Row,
   Col,
@@ -18,7 +19,7 @@ import * as authlib from '../lib/auth';
 export default class SignIn extends React.Component {
   constructor() {
     super();
-    this.state = { email: null };
+    this.state = { email: null, emailSent: false, error: null, loading: false };
     this.onSubmit = this.onSubmit.bind(this)
     this.onEmailChange = this.onEmailChange.bind(this);
     this.getEmailValidationState = this.getEmailValidationState.bind(this)
@@ -26,7 +27,15 @@ export default class SignIn extends React.Component {
 
   async onSubmit(event) {
     event.preventDefault();
-    await authlib.signIn(this.state.email);
+    this.setState({ loading: true });
+    try {
+      await authlib.signIn(this.state.email);
+      this.setState({ emailSent: true, error: null });
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ loading: false });
+    }
   }
 
   onEmailChange(event) {
@@ -46,24 +55,46 @@ export default class SignIn extends React.Component {
         <Row>
           <Col md={12}><h2>Sign-in</h2></Col>
         </Row>
-        <Form horizontal onSubmit={this.onSubmit}>
-          <Col md={8}>
-            <TextField
-              id="email"
-              label="Email Address"
-              onChange={this.onEmailChange.bind(this)}
-              validationState={this.getEmailValidationState()}
-              placeholder="you@youremail.domain" />
-            <hr />
-            <FormGroup>
-              <Col smOffset={3} sm={9}>
-                <Button bsStyle="primary" type="submit">
-                  Sign In
-                </Button>
-              </Col>
-            </FormGroup>
-          </Col>
-        </Form>
+        { this.state.error &&
+          <Row>
+            <Col md={12}>
+              <Alert bsStyle="warning">
+                <strong>Oh no!</strong> we couldn't get you in.
+                There's an error using this email address:
+                {' '}
+                <strong>{this.state.error}</strong>
+              </Alert>
+            </Col>
+          </Row> }
+        { this.state.emailSent &&
+          <Row>
+            <Col md={12}>
+              <Alert bsStyle="info">
+                <strong>Awesome!</strong> We do know that email! Please
+                go check your email and click in the link to get you in!
+                Feel free to close this page.
+              </Alert>
+            </Col>
+          </Row> }
+        { !this.state.emailSent &&
+          <Form horizontal onSubmit={this.onSubmit}>
+            <Col md={8}>
+              <TextField
+                id="email"
+                label="Email Address"
+                onChange={this.onEmailChange.bind(this)}
+                validationState={this.getEmailValidationState()}
+                placeholder="you@youremail.domain" />
+              <hr />
+              <FormGroup>
+                <Col smOffset={3} sm={9}>
+                  <Button bsStyle="primary" type="submit">
+                    Sign In
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Col>
+          </Form> }
       </Grid>
     );
   }
