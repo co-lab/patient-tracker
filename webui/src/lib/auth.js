a24247b393f52043724690170c60e4a8720a5ed2
Lincoln de Sousa <lincoln@comum.org>
2018-02-24 17:33:18 -0500

    client: Most of the signin flow working
    This change introduces the handling of the submit event in the SignIn
    component and the asynchronous request made to the /signin endpoint on
    the API. It's missing error handling (with UI) and storing the token.

--- webui/src/lib/auth.js
+++ webui/src/lib/auth.js
@@ -1,3 +1,17 @@
+export async function signIn(email) {
+  const fetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email }),
+  };
+  try {
+    const response = await fetch('http://localhost:8001/signin', fetchOptions);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export function isLoggedIn() {
-  return true;
+  return !!window.localStorage.authToken;
 }
--- webui/src/screens/SignIn.js
+++ webui/src/screens/SignIn.js
@@ -12,27 +12,59 @@ import {
 } from 'react-bootstrap';
 
 import TextField from '../components/TextField';
+import { isEmail } from '../../../server/src/lib/validation'; /* LoL */
+import * as authlib from '../lib/auth';
 
-export default () => (
-  <Grid style={{ paddingTop: 40 }}>
-    <Row>
-      <Col md={12}><h2>Sign-in</h2></Col>
-    </Row>
-    <Form horizontal>
-      <Col md={8}>
-        <TextField
-          id="email"
-          label="Email Address"
-          placeholder="you@youremail.domain" />
-        <hr />
-        <FormGroup>
-          <Col smOffset={3} sm={9}>
-            <Button bsStyle="primary" type="submit">
-              Sign In
-            </Button>
+export default class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = { email: null };
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.getEmailValidationState = this.getEmailValidationState.bind(this)
+  }
+
+  async onSubmit(event) {
+    event.preventDefault();
+    await authlib.signIn(this.state.email);
+  }
+
+  onEmailChange(event) {
+    const email = event.target.value;
+    this.setState({ email });
+  }
+
+  getEmailValidationState() {
+    if (!this.state.email) return null; /* Field wasn't touched yet */
+    try { isEmail(this.state.email); return null; } /* Success */
+    catch (error) { return 'error'; }
+  }
+
+  render() {
+    return (
+      <Grid style={{ paddingTop: 40 }}>
+        <Row>
+          <Col md={12}><h2>Sign-in</h2></Col>
+        </Row>
+        <Form horizontal onSubmit={this.onSubmit}>
+          <Col md={8}>
+            <TextField
+              id="email"
+              label="Email Address"
+              onChange={this.onEmailChange.bind(this)}
+              validationState={this.getEmailValidationState()}
+              placeholder="you@youremail.domain" />
+            <hr />
+            <FormGroup>
+              <Col smOffset={3} sm={9}>
+                <Button bsStyle="primary" type="submit">
+                  Sign In
+                </Button>
+              </Col>
+            </FormGroup>
           </Col>
-        </FormGroup>
-      </Col>
-    </Form>
-  </Grid>
-);
+        </Form>
+      </Grid>
+    );
+  }
+}
